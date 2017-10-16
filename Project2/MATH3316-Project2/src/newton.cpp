72391681e076a438f3faa326d7c5a0e490a3ccc6
Andy <andy.rash1996@gmail.com>
2017-10-16 11:02:27 -0500

    Added solution to Part 1 of the assignment (Newton's root approximations)

--- Project2/MATH3316-Project2/src/newton.cpp
+++ Project2/MATH3316-Project2/src/newton.cpp
@@ -6,4 +6,36 @@
 //  Copyright © 2017 andy. All rights reserved.
 //
 
-#include <stdio.h>
+#include <cmath>
+#include <iostream>
+
+using Fcn = std::function<double(double)>;
+
+double newton(Fcn& f, Fcn& df,
+              double x, std::size_t maxit,
+              double tol, bool show_iterates) {
+    
+    double prev = x;
+    
+    for(std::size_t i = 1; i < maxit; i++) {
+        
+        if(show_iterates) {
+            
+            std::cout << "iter "     << i                  << ", "
+                      << "x = "      << x                  << ", "
+                      << "|h| = "    << std::abs(prev - x) << ", "
+                      << "|f(x)| = " << f(x)               << std::endl;
+            
+        }
+        
+        // end iteration when within given tolerance
+        if(tol > std::abs(f(x) / df(x))) { break; }
+        
+        prev = x;
+        x = x - (f(x) / df(x));
+        
+    }
+    
+    return x;
+    
+}
--- Project2/MATH3316-Project2/src/test_newton.cpp
+++ Project2/MATH3316-Project2/src/test_newton.cpp
@@ -6,4 +6,38 @@
 //  Copyright © 2017 andy. All rights reserved.
 //
 
-#include <stdio.h>
+#include <cmath>
+#include <iostream>
+
+#include "newton.cpp"
+
+int main(int argc, const char* argv[]) {
+    
+    Fcn f = [](double x) -> double {
+        return (std::pow(x, 2) * (x - 3) * (x + 2));
+    };
+    
+    Fcn df = [](double x) -> double {
+        return ((4 * std::pow(x, 3)) - (3 * std::pow(x, 2)) - (12 * x));
+    };
+    
+    std::size_t max_iter = 50;
+    
+    for(double x_0 : {-3.0, 1.0, 2.0}) {
+        
+        for(double eps : {1.e-1, 1.e-5, 1.e-9}) {
+            
+            std::cout << "x_0 = " << x_0 << std::endl;
+            std::cout << "eps = " << eps << std::endl;
+            
+            double x = newton(f, df, x_0, max_iter, eps, true);
+            
+            std::cout << "The approximate root is: " << x << std::endl << std::endl;
+            
+        }
+        
+    }
+    
+    return 0;
+    
+}
