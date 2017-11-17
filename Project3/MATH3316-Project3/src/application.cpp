8e82abfa650897857919aa64488c56796022590f
Andy <andy.rash1996@gmail.com>
2017-11-16 19:25:29 -0600

    added basic main() function for application.cpp

--- Project3/MATH3316-Project3/src/application.cpp
+++ Project3/MATH3316-Project3/src/application.cpp
@@ -1,9 +1,19 @@
 //
 //  application.cpp
-//  MATH3316-Project3_Part1
+//  MATH3316-Project3_Part3
 //
 //  Created by andy on 11/7/17.
 //  Copyright © 2017 andy. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+
+#include "lagrange_interpolator.hpp"
+
+int main(int argc, char* argv[]) {
+    
+    
+    
+    return 0;
+    
+}
