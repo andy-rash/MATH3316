eb4fe29330277bad7b6b6558490b597df851bc74
Andy <andy.rash1996@gmail.com>
2017-11-29 16:16:42 -0600

    current state of test_basic_rules, since the instructions are unclear how to proceed

--- Project4/MATH3316-Project4/src/part1/test_basic_rules.cpp
+++ Project4/MATH3316-Project4/src/part1/test_basic_rules.cpp
@@ -6,9 +6,53 @@
 //  Copyright © 2017 andy. All rights reserved.
 //
 
-#include <iostream>
+#include <cmath>
+
+#include "numerical_integrator.hpp"
 
 int main(int argc, const char* argv[]) {
+    
+    // Function declarations
+    
+    Fcn polynomial = [&](double x) -> double {
+        return ((6 * std::pow(x, 2)) - (4 * x) + 1);
+    };
+
+    Fcn polynomial_antiderivative = [&](double x) -> double {
+        return ((2 * std::pow(x, 3)) - (2 * std::pow(x, 2)) + x);
+    };
+    
+    Fcn trig = [&](double x) -> double {
+        return ((std::sin(M_PI * x)) * (std::exp(x)));
+    };
+    
+    double trig_antiderivative = ((M_PI * (std::exp(1) - (1 / std::exp(1)))) / (1 + (std::pow(M_PI, 2))));
+    
+    NumericalIntegrator n_integ;
+    double a = -1.0;
+    double b = 2.0;
+    
+    double I_true = polynomial_antiderivative(b) - polynomial_antiderivative(a);
+    
+    std::vector<int> n_vals({20, 40, 80, 160, 320, 640});
+    for(std::size_t i = 0; i < n_vals.size(); i++) {
+        
+        double poly_integ = n_integ.composite_simpson(polynomial, a, b, n_vals[i]);
+        double poly_integ_err = std::abs(I_true - poly_integ);
+
+        std::cout << "Polynomial: "<< n_vals[i] << " " << poly_integ << " " << poly_integ_err << std::endl;
+
+        poly_integ = n_integ.composite_trapezoidal(polynomial, a, b, n_vals[i]);
+        poly_integ_err = std::abs(I_true - poly_integ);
+
+        std::cout << "Polynomial: "<< n_vals[i] << " " << poly_integ << " " << poly_integ_err << std::endl;
+        
+//        double trig_integ_simp = n_integ.composite_simpson(trig, -1.0, 1.0, n_vals[i]);
+//        double trig_integ_err = std::abs(trig_antiderivative - trig_integ_simp);
+//
+//        std::cout << "Trig: "<< n_vals[i] << " " << trig_integ_simp << " " << trig_antiderivative << " " << trig_integ_err << std::endl;
+        
+    }
     
     return 0;
 
