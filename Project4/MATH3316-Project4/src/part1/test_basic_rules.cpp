4ba71b141edff801d1163c6d5c6806babc0590bb
Andy <andy.rash1996@gmail.com>
2017-11-30 12:04:31 -0600

    output current state of part 1, pending review by professor

--- Project4/MATH3316-Project4/src/part1/test_basic_rules.cpp
+++ Project4/MATH3316-Project4/src/part1/test_basic_rules.cpp
@@ -31,26 +31,76 @@ int main(int argc, const char* argv[]) {
     NumericalIntegrator n_integ;
     double a = -1.0;
     double b = 2.0;
+    int n = 1000;
     
     double I_true = polynomial_antiderivative(b) - polynomial_antiderivative(a);
+    double I_approx_simp = n_integ.composite_simpson(polynomial, a, b, n);
+    double I_approx_trapezoid = n_integ.composite_trapezoidal(polynomial, a, b, n);
+    
+    double I_approx_simp_err = std::abs(I_true - I_approx_simp);
+    double I_approx_trapezoid_err = std::abs(I_true - I_approx_trapezoid);
+    
+    printf("True Integral (polynomial) = %22.16e\n\n", I_true);
+    
+    std::cout << "Simpson's Rule Polynomial test (f(x) = 6x^2 - 4x + 1):" << std::endl;
+    std::cout << "     n             R(f)               abs err" << std::endl;
+    std::cout << "-------------------------------------------------------" << std::endl;
+    printf("  %6i    %22.16e    %7.1e         \n\n", n, I_approx_simp, I_approx_simp_err);
+    
+    std::cout << "Trapezoid Rule Polynomial test (f(x) = 6x^2 - 4x + 1):" << std::endl;
+    std::cout << "     n             R(f)               abs err" << std::endl;
+    std::cout << "-------------------------------------------------------" << std::endl;
+    printf("  %6i    %22.16e    %7.1e         \n\n\n", n, I_approx_trapezoid, I_approx_trapezoid_err);
+    
+    
+    printf("True Integral (trig function) = %22.16e\n", trig_antiderivative);
+    
+    std::cout << "\n Simpson Rule approximation:\n";
+    std::cout << "     n             R(f)            abs err    conv rate\n";
+    std::cout << "  -----------------------------------------------------\n";
     
     std::vector<int> n_vals({20, 40, 80, 160, 320, 640});
+    std::vector<double> errors;
+    std::vector<double> h_vals;
+    a = -1.0;
+    b = 1.0;
     for(std::size_t i = 0; i < n_vals.size(); i++) {
         
-        double poly_integ = n_integ.composite_simpson(polynomial, a, b, n_vals[i]);
-        double poly_integ_err = std::abs(I_true - poly_integ);
-
-        std::cout << "Polynomial: "<< n_vals[i] << " " << poly_integ << " " << poly_integ_err << std::endl;
-
-        poly_integ = n_integ.composite_trapezoidal(polynomial, a, b, n_vals[i]);
-        poly_integ_err = std::abs(I_true - poly_integ);
-
-        std::cout << "Polynomial: "<< n_vals[i] << " " << poly_integ << " " << poly_integ_err << std::endl;
+        double trig_integ_simp = n_integ.composite_simpson(trig, a, b, n_vals[i]);
+        errors.push_back(std::abs(trig_antiderivative - trig_integ_simp));
+        h_vals.push_back((double)((b - a) / n_vals[i]));
         
-//        double trig_integ_simp = n_integ.composite_simpson(trig, -1.0, 1.0, n_vals[i]);
-//        double trig_integ_err = std::abs(trig_antiderivative - trig_integ_simp);
-//
-//        std::cout << "Trig: "<< n_vals[i] << " " << trig_integ_simp << " " << trig_antiderivative << " " << trig_integ_err << std::endl;
+        printf("   %6i", n_vals[i]);
+        
+        if(i == 0) {
+            printf("  %22.16e  %7.1e     ----\n", trig_integ_simp, errors[i]);
+        } else {
+            printf("  %22.16e  %7.1e   %f\n", trig_integ_simp, errors[i],
+                   (std::log(errors[i-1]) - std::log(errors[i])) / (std::log(h_vals[i-1]) - std::log(h_vals[i])));
+        }
+        
+    }
+    
+    std::cout << "\n Trapezoid Rule approximation:\n";
+    std::cout << "     n             R(f)            abs err    conv rate\n";
+    std::cout << "  -----------------------------------------------------\n";
+    
+    errors.clear();
+    h_vals.clear();
+    for(std::size_t i = 0; i < n_vals.size(); i++) {
+        
+        double trig_integ_trapezoid = n_integ.composite_trapezoidal(trig, a, b, n_vals[i]);
+        errors.push_back(std::abs(I_true - trig_integ_trapezoid));
+        h_vals.push_back((double)((b - a) / n_vals[i]));
+        
+        printf("   %6i", n_vals[i]);
+        
+        if(i == 0) {
+            printf("  %22.16e  %7.1e     ----\n", trig_integ_trapezoid, errors[i]);
+        } else {
+            printf("  %22.16e  %7.1e   %f\n", trig_integ_trapezoid, errors[i],
+                   (std::log(errors[i-1]) - std::log(errors[i])) / (std::log(h_vals[i-1]) - std::log(h_vals[i])));
+        }
         
     }
     
